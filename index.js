62a135d73abb1579edec5a290c3f72d1b30d00c8
Renato Albuquerque <renatomalb83@gmail.com>
2023-05-29 11:37:34 -0300

    started js code

---
+++ index.js
@@ -0,0 +1,17 @@
+// VARIABLES
+
+const hamburgerMenu = document.querySelector(".imageMenuHamburger")
+const menuDropdownMobile = document.querySelector(".menuDropdownMobile")
+
+
+// FUNCTIONS
+
+function showMobileMenu() {
+    menuDropdownMobile.classList.toggle("open")
+
+    if (menuDropdownMobile.classList.contains("open")) {
+        hamburgerMenu.src = "files/images/icon-close.svg"
+    } else {
+        hamburgerMenu.src = "files/images/icon-hamburger.svg"
+    }
+}
