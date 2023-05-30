51d77e40ca793b521d4fc3f45bb318ed175ed19b
Renato Albuquerque <renatomalb83@gmail.com>
2023-05-30 10:49:11 -0300

    updated functions js - mobile

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 const hamburgerMenu = document.querySelector(".imageMenuHamburger")
 const menuDropdownMobile = document.querySelector(".menuDropdownMobile")
+const mobileItem = document.querySelector(".mobileItem")
+const mobileSubitemProduct = document.querySelector(".mobileSubitemProduct")
+const mobileSubitemCompany = document.querySelector(".mobileSubitemCompany")
+const mobileSubitemConnect = document.querySelector(".mobileSubitemConnect")
+const arrowProduct = document.querySelector(".arrowProduct")
+const arrowCompany = document.querySelector(".arrowCompany")
+const arrowConnect = document.querySelector(".arrowConnect")
 
 // FUNCTIONS
 
@@ -15,3 +22,17 @@ function showMobileMenu() {
     }
 }
 
+function showProductSubitem() {
+    mobileSubitemProduct.classList.toggle("mobileSubitemProductOpen")
+    arrowProduct.classList.toggle("arrow")
+}
+
+function showCompanySubitem() {
+    mobileSubitemCompany.classList.toggle("mobileSubitemCompanyOpen")
+    arrowCompany.classList.toggle("arrow")
+}
+
+function showConnectSubitem() {
+    mobileSubitemConnect.classList.toggle("mobileSubitemConnectOpen")
+    arrowConnect.classList.toggle("arrow")
+}
